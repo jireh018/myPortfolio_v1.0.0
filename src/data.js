eef10108b25bf09e5413a1d5154f8e23137d559d
jireh018 <jirehtuto71@gmail.com>
2023-04-14 12:51:08 -0300

    portfolio

---
+++ src/data.js
@@ -0,0 +1,60 @@
+export const projects = [
+  {
+    title: "JOBIFY",
+    subtitle: "MERN Stack(React JS and Node JS)",
+    description:
+      "a full stack application realized with Node js and React Js that can be used for jobs : adding, deleting, updating and posting jobs and also updating the user profile.",
+    image: "./jobster.gif",
+    link: "https://nodecourse-jobster-api.onrender.com/landing",
+  },
+  {
+    title: "JOBS API",
+    subtitle: "NODE JS AND MONGO DB",
+    description:
+      "A simple web api that helps the user to register and login and create Jobs and also modify them, delete them, post them and get them and use the LocalStorage to store the user info",
+    image: "./jobs-api.gif",
+    link: "https://reactbootcamp.com",
+  },
+  {
+    title: "E-COMMERCE API",
+    subtitle: "NODE JS AnD MONGO DB",
+    description:
+      "a e-commerce web api that uses Authentication and maintain his session with cookies, the user then can operate CRUD operation on products, make an order and leave a rating and comment on the product.",
+    image: "./e-commerce.gif",
+    link: "https://reactbootcamp.com",
+  },
+  /*{
+    title: "Epic Todo App",
+    subtitle: "React Hooks",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-4.gif",
+    link: "https://reactbootcamp.com",
+  },*/
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+    image: "https://randomuser.me/api/portraits/men/1.jpg",
+    name: "Jesse Hicks",
+    company: "Zoozle",
+  },
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+    image: "https://randomuser.me/api/portraits/men/94.jpg",
+    name: "Ruben Alvarez",
+    company: "Dooble.io",
+  },
+];
+
+export const skills = [
+  "JavaScript",
+  "React",
+  "Next.js",
+  "Node",
+  "GraphQL",
+  "Salesforce"
+];

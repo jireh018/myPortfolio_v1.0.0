56c0815c13238ce4bb48490eb06d4252f4021fb9
jireh018 <jirehtuto71@gmail.com>
2023-04-14 16:02:18 -0300

    yarn deleted

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -24,7 +24,6 @@ const Contact = () => {
             .catch((error) => alert(error));
     }
 
-
     return (
         <section id="contact" className="relative">
             <div className="container px-5 py-10 mx-auto flex sm:flex-nowrap flex-wrap">
